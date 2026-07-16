401f3143b4c798ddab7276066a46d01ac02ae56d
Niharika Chawla <chawlaniharika23@gmail.com>
2026-07-17 00:11:37 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Mascot from "./components/Mascot/Mascot";
+
+function App() {
+  return (
+    <div className="app">
+      <Mascot />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Mascot/Mascot.jsx
@@ -0,0 +1,15 @@
+import idle1 from "../../assets/mascot/idle/idle1.png";
+
+function Mascot() {
+  return (
+    <img
+      src={idle1}
+      alt="Mascot"
+      style={{
+        width: "180px",
+      }}
+    />
+  );
+}
+
+export default Mascot;
