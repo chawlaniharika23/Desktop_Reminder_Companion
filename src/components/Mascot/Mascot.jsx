6ec047aa30b8ebd8c0a64587a90fa07768a0d257
Niharika Chawla <chawlaniharika23@gmail.com>
2026-07-17 13:41:55 +0530

    Adding animations

--- src/components/Mascot/Mascot.jsx
+++ src/components/Mascot/Mascot.jsx
@@ -1,10 +1,25 @@
+import SpriteAnimator from "./SpriteAnimator";
+
 import idle1 from "../../assets/mascot/idle/idle1.png";
-import "./Mascot.css";
+import idle2 from "../../assets/mascot/idle/idle2.png";
+import idle3 from "../../assets/mascot/idle/idle3.png";
+import idle4 from "../../assets/mascot/idle/idle4.png";
+
+const idleFrames = [
+  idle1,
+  idle2,
+  idle3,
+  idle4,
+];
 
 function Mascot() {
   return (
     <div className="mascot">
-      <img src={idle1} alt="Desktop Companion" />
+      <SpriteAnimator
+        frames={idleFrames}
+        frameDuration={180}
+        width={180}
+      />
     </div>
   );
 }
---
+++ src/components/Mascot/SpriteAnimator.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function SpriteAnimator({ frames, frameDuration = 10, width = 180 }) {
+  const [currentFrame, setCurrentFrame] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentFrame((prev) => (prev + 1) % frames.length);
+    }, frameDuration);
+
+    return () => clearInterval(interval);
+  }, [frames, frameDuration]);
+
+  return (
+    <img
+      src={frames[currentFrame]}
+      alt="Mascot"
+      style={{ width }}
+    />
+  );
+}
+
+export default SpriteAnimator;
