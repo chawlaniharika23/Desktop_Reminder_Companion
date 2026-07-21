032c859d5c6f5967303fd800b6da6cd9b6e8d705
Niharika Chawla <chawlaniharika23@gmail.com>
2026-07-21 20:17:12 +0530

    Made the character reach a target and stop

--- src/hooks/useMovement.js
+++ src/hooks/useMovement.js
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
 
 function useMovement(state) {
-  const [x, setX] = useState(window.innerWidth);
+  const START_X = window.innerWidth;
+  const TARGET_X = 900;
+  const SPEED = 2;
+
+  const [x, setX] = useState(START_X);
 
   useEffect(() => {
     if (state !== "walk") return;
 
     const interval = setInterval(() => {
-      setX((prev) => prev - 2);
+      setX((prev) => {
+        if (prev <= TARGET_X) {
+          return TARGET_X;
+        }
+
+        return prev - SPEED;
+      });
     }, 16);
 
     return () => clearInterval(interval);
