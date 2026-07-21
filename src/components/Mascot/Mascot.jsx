f524b181b17af1a87ca6bbd5b3d75902c9239547
Niharika Chawla <chawlaniharika23@gmail.com>
2026-07-21 20:04:16 +0530

    Making the character move across the screen

--- src/components/Mascot/Mascot.jsx
+++ src/components/Mascot/Mascot.jsx
@@ -1,5 +1,5 @@
 import "./Mascot.css";
-
+import useMovement from "../../hooks/useMovement";
 import SpriteAnimator from "./SpriteAnimator";
 import useAnimation from "../../hooks/useAnimation";
 import useMascotState from "../../hooks/useMascotState";
@@ -7,9 +7,15 @@ import useMascotState from "../../hooks/useMascotState";
 function Mascot() {
   const state = useMascotState();
   const animation = useAnimation(state);
+  const x = useMovement(state);
 
   return (
-    <div className="mascot">
+    <div
+      className="mascot"
+      style={{
+        transform: `translateX(${x}px)`
+      }}
+    >
       <SpriteAnimator
         frames={animation.frames}
         frameDuration={animation.fps}
--- src/hooks/useMovement.js
+++ src/hooks/useMovement.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
 function useMovement(state) {
-  const [x, setX] = useState(0);
+  const [x, setX] = useState(-200);
 
   useEffect(() => {
     if (state !== "walk") return;
 
     const interval = setInterval(() => {
-      setX((prev) => prev + 3);
+      setX((prev) => prev + 2);
     }, 16);
 
     return () => clearInterval(interval);
